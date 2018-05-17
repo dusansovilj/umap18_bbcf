7d2f74ae922e1c350bc5f53700bf53c4e6132a32
dusansovilj <parmafan@gmail.com>
2018-05-17 15:53:03 -0400

    modular bbcf module

--- algs/bbcf_modular.py
+++ algs/bbcf_modular.py
@@ -6,12 +6,11 @@ BBCF main module
 import numpy as np
 from scipy.sparse import coo_matrix
 
-from math import exp,log
+from math import exp
 
-from algs.utils import get_next_state_probability
 from algs.utils import get_goal_state_distribution
-
 from algs.wrapper import Method
+from algs.markov import MethodMarkov
 
 
 class MethodBBCF(Method):
@@ -19,7 +18,8 @@ class MethodBBCF(Method):
     def __init__(self, num_states, adjacency_list, reg_factor, num_models):
         super(MethodBBCF, self).__init__(num_states, adjacency_list)
         self._rf = reg_factor
-        self._num_models = num_models   # number of models to use (limit to most dominant final states) 
+        self._num_models = num_models   # number of models to use (limit to most dominant final states)
+        self._models = {}   # list of Method objects 
     
     
     def name(self):
@@ -34,15 +34,18 @@ class MethodBBCF(Method):
                                                       self._num_states, 
                                                       data)
         
-        self._T = get_goal_transition_matrices(data, 
-                                               self._num_states, 
-                                               self._goal_state_label, 
-                                               self._adjacency_list, 
-                                               self._rf)
-        
-        self._initial_state_prob = get_initial_state_probability(data, 
-                                                                 self._num_states, 
-                                                                 self._rf)
+        # modify the model initialization to accommodate 
+        # different learning algorithms
+        # initialize models
+        for goal in self._goal_state_label:
+            temp_model = MethodMarkov(
+                self._num_states, 
+                self._adjacency_list, 
+                self._rf)
+            temp_data = [s  for s in data if s[-1] == goal]
+#             temp_data = temp_model.prepare_data(temp_data)
+            temp_model.train(temp_data)
+            self._models[goal] = temp_model
         
         self._goal_prior_prob = P_T_frequency(data,
                                               self._num_states)
@@ -54,31 +57,6 @@ class MethodBBCF(Method):
         res, loglikes = self.P_a_c(sequence[0][:-1], args[0])  # expect log-likelihoods here
         return res, loglikes
         
-        
-    # P(a | C)
-#     def P_a_c(self, trajectory):
-#         '''
-#         Return the next_state probability given the prior_trajectory
-#         Input: training data, number_of_states(400 or 14),number_of_task(400 or 4)
-#                epsilon(regularization parameter),
-#                T_res(contains all goal-transition matrix)
-#                Initial_state_probability(P(C_0))
-#                frequency(P(T_g))
-#         '''
-#         res = np.zeros(self._num_states)
-#         
-#         P_C, likelihood_record = self.p_c(trajectory)      # record the likelihood of the trajectory
-#         
-#         # Sum over all goal-states
-#         weight_record = []
-#         for goal in self._goal_state_label:
-#             x = self.P_a_TC(goal, trajectory)              # P(a|T_g,C) probability of next_state
-#             y = self.P_T_C(goal, likelihood_record, P_C)   # P(T|C) weights for different goal-states
-#             res += x * y 
-#             weight_record.append(y)
-#             
-#         loglikes = [log(likes) if likes > 0 else log(self._rf) for likes in likelihood_record]
-#         return res, weight_record, loglikes
     
     # P(a | c) - probability of an action given a trajectory
     def P_a_c(self, trajectory, loglikes=None):
@@ -87,32 +65,18 @@ class MethodBBCF(Method):
         
         for goal in self._goal_state_label:
             x = self.P_a_TC(goal, trajectory)
-            y = self.P_T_C_v2(goal, loglikes, P_C)
+            y = exp(loglikes[goal]) * self._goal_prior_prob[goal] / P_C  # P(C|T) * P(T) / P(C) = P(T|C) - posterior of a model
             res += x * y
         return res, loglikes
     
     
-#     # P(c) - probability of a trajectory
-#     def p_c(self, trajectory):
-#         ''' Calculate the P(C) and record the likelihood for other computation '''
-#         res = 0
-#         likelihood_record = np.zeros(self._num_states)
-#         
-#         for goal in self._goal_state_label:
-#             log_likelihood = self.log_P_C_T(goal, trajectory)
-#             likelihood = exp(log_likelihood)
-#             likelihood_record[goal] += (likelihood)
-#             res += likelihood * self._goal_prior_prob[goal]
-#             
-#         return res, likelihood_record
-    
-    
     # P(c) - probability of a trajectory
+    # P(c) = sum_{t=1}^T P(t) * P(c|t),  
+    #        prior goal model probability * likelihood under that goal model
     def p_c(self, trajectory, loglikes=None):
         res = 0
         if loglikes is None:
             loglikes = np.zeros(self._num_states)
-            
             for goal in self._goal_state_label:
                 loglikes[goal] += self.log_P_C_T(goal, trajectory)
                 res += exp(loglikes[goal]) * self._goal_prior_prob[goal]
@@ -125,81 +89,30 @@ class MethodBBCF(Method):
         return res, loglikes
     
     
-#     def log_P_C_T(self, goal_state, trajectory):
-#         '''Get the log likelihood of the prior trajectory'''
-#         log_sum = 0
-#         T = self._T[goal_state]
-#         
-#         # P(C_i | C_{i-1}, T_g)
-#         for i in range(len(trajectory) - 1):
-#             previous_state = trajectory[i]
-#             action_probability = get_next_state_probability(T, previous_state)  #get probability
-#             if action_probability[trajectory[i+1]] > 0:
-#                 log_sum += log(action_probability[trajectory[i+1]])
-#             else:
-#                 log_sum += log(self._rf)
-#                 
-#         # P(C_0)
-#         p_c_one = self._initial_state_prob[goal_state][trajectory[0]]
-#         log_p_c_one = log(p_c_one)
-#         log_sum += log_p_c_one
-#         return log_sum
-    
+    def loglike(self, sequence, *args):
+        full_loglike, _ = self.p_c(sequence, args[0])
+        return full_loglike 
+
     
     def log_P_C_T(self, goal_state, trajectory, loglike=None):
         '''
-        Return log-likelihood of a trajectory given transition model. Add the last state
-        of trajectory into log-like computation given specific goal state.
-        If log-likelihood is not provided, it is computed from scratch. 
+        Return log-likelihood of a trajectory given transition model. 
+        If log-likelihood is not provided, it should be computed from scratch. 
         '''
-        if loglike is None:
-            # P(C_0)
-            p_c_one = self._initial_state_prob[goal_state][trajectory[0]]
-            log_p_c_one = log(p_c_one)
-            log_sum = log_p_c_one
-            start_pos = 0
-        else:
-            log_sum = loglike
-            start_pos = len(trajectory) - 2
-    
-        T = self._T[goal_state]
+        return self._models[goal_state].loglike(trajectory, loglike)
         
-        # P(C_i | C_{i-1}, T_g)
-        for i in range(start_pos, len(trajectory) - 1):
-            previous_state = trajectory[i]
-            action_probability = get_next_state_probability(T, previous_state)
-            log_sum += log(action_probability[trajectory[i+1]])  if action_probability[trajectory[i+1]] > 0 else log(self._rf)
-            
-        return log_sum
-        
-    
     
     # P(a | T_g, C)
     def P_a_TC(self, goal_state, trajectory):
         '''
-        P(a|T_g,C) 
-        Get the next_state probability distribution based on the Goal_transition matrix 
-        and the last element from the prior-trajectory
+        Get the next_state probability distribution based on 
+        the destination state and trajectory.
         '''
-        T = self._T[goal_state]                                # Get goal-transition matrix from the collection of transition matrix
-        action = get_next_state_probability(T, trajectory[-1]) # Get next_state prob. dist. given T and last element from prior_trajectory
+        temp_trajectory = [trajectory + [None]]
+        temp_data = self._models[goal_state].prepare_data(temp_trajectory)
+        action, _ = self._models[goal_state].predict(temp_data)
         return action
     
-    
-    # P(T_g | C)
-    def P_T_C(self, goal_state, likelihood_record, P_C):
-        '''
-        P(T|C) input goal_state and the likelihood_record collected by P(C)
-        Output the weights of next_action probability
-        '''
-        P_C_T = likelihood_record[goal_state]
-        P_T = self._goal_prior_prob[goal_state]
-        return P_C_T * P_T / P_C
-
-    def P_T_C_v2(self, goal_state, log_likelihood, P_C):
-        P_C_T = exp(log_likelihood[goal_state])
-        P_T = self._goal_prior_prob[goal_state]
-        return P_C_T * P_T / P_C
     
 
 def get_specific_goal_transition(train_data, num_of_state, goal_state_label, adjacent_state, epsilon):
@@ -285,10 +198,6 @@ def get_goal_transition_matrices(train_data, num_of_state, goal_state_label, adj
         else:
             T.append([])
             
-#     for idx in range(len(goal_state_label)):
-#         row_sums = res[idx].sum(axis=1)
-#         res[idx] = res[idx] / row_sums[:, np.newaxis]    
-        
     return T
 
 
@@ -301,7 +210,9 @@ def get_goal_state_label(first_k_goal, num_of_state, data):
 
 
 def get_initial_state_probability(data, num_states, epsilon):
-    '''Go over the training set and count the frequency of the start_state probability distribution'''
+    '''
+    Go over the training set and count the frequency of the start_state 
+    probability distribution for each destination '''
     distribution = np.zeros((num_states, num_states))
     for i in range(num_states):
         for j in range(len(data)):
@@ -314,6 +225,8 @@ def get_initial_state_probability(data, num_states, epsilon):
     row_sums = distribution.sum(axis=1)
     new_matrix = distribution / row_sums[:, np.newaxis]
     return new_matrix
+
+    
 
 
 def P_T_frequency(train_data, max_goals):
